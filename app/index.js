c3df3b0003cd1d4cb53e4057aeaedae20d257463
ntwcklng <ntwcklng@gmail.com>
2016-11-17 10:18:02 +0100

    progress

--- app/index.js
+++ app/index.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   StyleSheet,
-  Text,
   View
 } from 'react-native';
 
+import GameView from './Views/GameView';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
   },
 });
 
-export default class App extends Component {
-  render() {
-    return (
-      <View><Text>Hallo</Text></View>
-    );
-  }
-}
+const TapWar = function() {
+  return (
+    <View style={styles.container}>
+      <GameView />
+    </View>
+  );
+};
+export default TapWar;
