b27a8de98bf24841b851af2efd0f1202b5a34e41
ntwcklng <ntwcklng@gmail.com>
2016-11-23 14:32:38 +0100

    lint

--- app/Views/GameView.js
+++ app/Views/GameView.js
@@ -5,19 +5,18 @@ import {
   TouchableOpacity,
   Text,
   Dimensions,
-  Animated,
+  Animated
 } from 'react-native';
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'stretch',
-    flexDirection: 'column',
+    flexDirection: 'column'
   },
   tapField: {
-    flex:1,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'stretch',
     flexDirection: 'column'
@@ -25,12 +24,13 @@ const styles = StyleSheet.create({
   tapFieldText: {
     fontSize: 36,
     color: 'rgba(255,255,255,.3)',
-    alignSelf: 'center',
-  },
+    alignSelf: 'center'
+  }
+
 });
 
 export default class GameView extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       score: 0,
@@ -38,10 +38,10 @@ export default class GameView extends Component {
       activeGame: false,
       winner: '',
       height: Dimensions.get('window').height,
-      width: Dimensions.get('window').width,
-    }
+      width: Dimensions.get('window').width
+    };
   }
-  _handleTap(id) {
+  _handleTap (id) {
     const { score, activeGame, animScore } = this.state;
     if (!activeGame) return;
     let actualScore = score;
@@ -60,15 +60,15 @@ export default class GameView extends Component {
       }
     });
   }
-  _initGame() {
+  _initGame () {
     this.setState({
       activeGame: true,
       score: 0,
       animScore: new Animated.Value(0),
-      winner: '',
+      winner: ''
     });
   }
-  render() {
+  render () {
     const { score, winner, height, width, animScore, activeGame } = this.state;
     return (
       <View style={styles.container}>
@@ -78,22 +78,18 @@ export default class GameView extends Component {
         <TouchableOpacity onPress={() => this._handleTap(2)} style={[styles.tapField, {backgroundColor: '#e74c3c'}]}>
           <Text style={styles.tapFieldText}>{!winner && score} {winner && `${winner} hat Gewonnen!`}</Text>
         </TouchableOpacity>
-        <Animated.View style={
-          {
-            backgroundColor: '#ecf0f1',
-            borderRadius: 4,
-            width: 50,
-            height: 40,
-            left: (width / 2) - 25,
-            position: 'absolute',
-            top: animScore.interpolate({ inputRange: [-10, 10], outputRange: [0, height - 40] })
+        <Animated.View style={{backgroundColor: '#ecf0f1',
+          borderRadius: 4,
+          width: 50,
+          height: 40,
+          left: (width / 2) - 25,
+          position: 'absolute',
+          top: animScore.interpolate({ inputRange: [-10, 10], outputRange: [0, height - 40] })}}>
+          {!activeGame &&
+            <TouchableOpacity onPress={() => this._initGame()} style={{justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{alignSelf: 'center', lineHeight: 40}}>START</Text>
+            </TouchableOpacity>
           }
-        }>
-        {!activeGame &&
-          <TouchableOpacity onPress={() => this._initGame()} style={{justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{alignSelf: 'center', lineHeight: 40}}>START</Text>
-          </TouchableOpacity>
-        }
         </Animated.View>
       </View>
     );
